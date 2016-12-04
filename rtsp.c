10a826ed65ffcbd80f56ec19fa163f42a83a39bc
yuma25689 <yuma25689@gmail.com>
2016-12-05 02:12:25 +0900

    log output

--- rtsp.c
+++ rtsp.c
@@ -1380,6 +1380,7 @@ static void handle_set_parameter(rtsp_conn_info *conn, rtsp_message *req,
         }
         else
         {
+          debug(1, "write picture req->contentlength:%ld\n", req->contentlength);
           write(fdImage, req->content, req->contentlength);
           close(fdImage);
         }
